46347061c4e8291a08f56ee5a6531fb57f200e26
Ray Hawthorne <frontogenesis@gmail.com>
2018-10-27 19:51:50 -0500

    Fix WeatherSTEM config issue

--- routes/weatherstem.js
+++ routes/weatherstem.js
@@ -20,7 +20,7 @@ router.get('/stations/:id', (req, res, next) => {
   const id = req.params.id;
 
   weatherstem.post('https://leon.weatherstem.com/api', {
-  api_key: '2r67hetp', stations: [id]
+  api_key: 'noway', stations: [id]
 })
   .then((data) => {
     res.json({
