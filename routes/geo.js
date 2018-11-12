ba00faff10399e7d0d21849b6d9bd5eb73a73ae9
Ray Hawthorne <frontogenesis@gmail.com>
2018-11-12 14:08:34 -0600

    Add geo route

---
+++ routes/geo.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+const { ApiRequest } = require('./../middleware/fetch');
+
+router.get('/', (req, res, next) => {
+  res.send('Geo Endpoint');
+});
+
+router.get('/places/:location', async (req, res, next) => {
+  const location = req.params.location;
+
+  let uri = `https://maps.googleapis.com/maps/api/geocode/json?`
+  const uriParams = `address=${location}&key=${process.env.GOOGLE_PLACES_KEY}`;
+  uri = `${uri}${uriParams}`;
+  
+  const googlePlaces = new ApiRequest(uri);
+
+  try {
+    const data = await googlePlaces.get();
+    res.json({
+      lat: data.results[0].geometry.location.lat,
+      lon: data.results[0].geometry.location.lng
+    });
+  } catch(error) {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
