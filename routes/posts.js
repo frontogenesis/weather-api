1c6e552cf4a4956b703d20352baaf963e8d9060a
Ray Hawthorne <frontogenesis@gmail.com>
2020-12-04 20:05:29 -0500

    Add logic for password hashing

--- routes/posts.js
+++ routes/posts.js
@@ -3,31 +3,63 @@ const router = express.Router()
 
 const Post = require('../models/post')
 
-router.get('/', (req, res) => {
-    Post.find({})
-        .then(posts => res.send(posts))
-        .catch(error => res.status(500).send())
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find({})
+        res.send(posts)
+    } catch (error) {
+        res.status(500).send()
+    }
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
     const _id = req.params.id
 
-    Post.findById(_id)
-        .then(post => {
-            if (!post) {
-                return res.status(404).send()
-            }
-            res.send(post)
-        })
-        .catch(error => res.status(500).send())
+    try {
+        const post = await Post.findById(_id)
+        !post ? res.status(404).send() : res.send(post)
+    } catch (error) {
+        res.status(500).send()
+    }
 })
 
-router.post('/', (req, res) => {
+router.patch('/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['title', 'body']
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(404).send({ error: 'Invalid updates' })
+    }
+
+    try {
+        const post = await Post.findById(req.params.id)
+        updates.forEach(update => post[update] = req.body[update])
+        await post.save()
+        !post ? res.status(404).send() : res.send(post)
+    } catch (error) {
+        res.status(400).send()
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await Post.findByIdAndDelete(req.params.id)
+        !post ? res.status(404).send() : res.send(post)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+router.post('/', async (req, res) => {
     const post = new Post(req.body)
 
-    post.save()
-        .then(() => res.status(201).send(post))
-        .catch(error => res.status(400).send(error.message))
+    try {
+        await post.save()
+        res.status(201).send(post)
+    } catch(error) {
+        res.status(400).send(error.message)
+    }
 })
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -3,30 +3,63 @@ const router = express.Router()
 
 const User = require('../models/user')
 
-router.get('/', (req, res) => {
-  User.find({})
-    .then(users => res.send(users))
-    .catch(error => res.status(500).send())
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find({})
+    res.send(users)
+  } catch(error) {
+    res.status(500).send()
+  }
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const _id = req.params.id
-  
-  User.findById(_id).then((user) => {
-    if (!user) {
-      return res.status(404).send()
-    }
 
-    res.send(user)
-  }).catch(e => res.status(500).send())
+  try {
+    const user = await User.findById(_id)
+    !user ? res.status(404).send() : res.send(user)
+  } catch(error) {
+    res.status(500).send()
+  }
 })
 
-router.post('/', (req, res) => {
+router.patch('/:id', async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['name', 'email', 'password', 'age']
+  const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates' })
+  }
+
+  try {
+    const user = await User.findById(req.params.id)
+    updates.forEach(update => user[update] = req.body[update])
+    await user.save()
+    !user ? res.status(404).send() : res.send(user)
+  } catch(error) {
+    res.status(400).send()
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id)
+    !user ? res.status(404).send() : res.send(user)
+  } catch(error) {
+    res.status(500).send()
+  }
+})
+
+router.post('/', async (req, res) => {
   const user = new User(req.body)
 
-  user.save()
-    .then(() => res.status(201).send(user))
-    .catch(error => res.status(400).send(error.message))
+  try {
+    await user.save()
+    res.status(201).send(user)
+  } catch(error) {
+    res.status(400).send(error.message)
+  }
 })
 
 module.exports = router
